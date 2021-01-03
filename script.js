287a3a4f6a89bac9357792b2bc49e162f3c3f57b
MohamedBassiouny26 <mohamedsharf26797@gmail.com>
2021-01-03 20:46:35 +0200

    left move and left jump is added

--- script.js
+++ script.js
@@ -20,6 +20,8 @@ class Animate {
     }
     update() {
         this.count++;
+        console.log(this.count)
+        console.log(this.frame)
         // console.log(this.count)
         if (this.count >= this.delay) {
             this.count = 0;
@@ -64,27 +66,47 @@ class Character {
         this.width = width
         this.Frame_set = {
             idle: ["./img/monkey_idle.png", "./img/monkey_idle.png"],
+            idleLeft: ["./img/monkey_idle_left.png", "./img/monkey_idle_left.png"],
             walkRight: ["./img/monkey_walk_1.png", "./img/monkey_walk_2.png", "./img/monkey_walk_3.png", "./img/monkey_walk_4.png"],
-            jump: ["./img/monkey_jump_1.png", "./img/monkey_jump_2.png", "./img/monkey_jump_3.png", "./img/monkey_jump_4.png"],
-            walkleft: ["./img/monkey_walk_1.png", "./img/monkey_walk_3.png"],
+            walkLeft: ["./img/monkey_walkleft_1.png", "./img/monkey_walkleft_2.png", "./img/monkey_walkleft_3.png", "./img/monkey_walkleft_4.png"],
+            jumpRight: ["./img/monkey_jump_1.png", "./img/monkey_jump_2.png", "./img/monkey_jump_3.png", "./img/monkey_jump_4.png"],
+            jumpLeft: ["./img/monkey_jumpleft_1.png", "./img/monkey_jumpleft_2.png", "./img/monkey_jumpleft_3.png", "./img/monkey_jumpleft_4.png"],
         }
         this.animate = new Animate(this.Frame_set.idle, 15);
+        this.face = "right";
     }
     spirit() {
         if (controller.upActive && !this.jumping) {
             this.jumping = true;
             this.y_velocity -= 7;
             controller.upActive = false;
-            this.animate.change(this.Frame_set.jump, 15);
-        } else if (controller.rightActive) {
-            this.x_velocity += 0.05;
-            controller.rithtActive = false;
-            if (!this.jumping)
-                this.animate.change(this.Frame_set.walkRight, 15);
-        } else if (!controller.rightActive && !controller.leftActive) {
-            this.animate.change(this.Frame_set.idle, 15);
+            if (this.face == "right")
+                this.animate.change(this.Frame_set.jumpRight, 15);
+            else if (this.face == "left")
+                this.animate.change(this.Frame_set.jumpLeft, 15);
+
         }
-        this.y_velocity += 0.05;
+        if (controller.leftActive) {
+            this.face = "left"
+            this.x_velocity -= 0.05;
+            if (!this.jumping) {
+                this.animate.change(this.Frame_set.walkLeft, 15);
+            }
+        }
+        if (controller.rightActive) {
+            this.face = "right"
+            this.x_velocity += 0.05;
+            if (!this.jumping) {
+                this.animate.change(this.Frame_set.walkRight, 15);
+            }
+        }
+        if (!controller.rightActive && !controller.leftActive) {
+            if (this.face == "right")
+                this.animate.change(this.Frame_set.idle, 15);
+            else if (this.face == "left")
+                this.animate.change(this.Frame_set.idleLeft, 15);
+        }
+        this.y_velocity += 0.05; //used as a graphity
         this.xPosition += this.x_velocity;
         this.yPosition += this.y_velocity;
         this.x_velocity *= 0.9;
@@ -94,8 +116,13 @@ class Character {
             this.yPosition = ctx.canvas.height - player1.height;
             this.y_velocity = 0;
         }
-    }
+        if (this.xPosition + this.width < 0) {
+            this.xPosition = ctx.canvas.width;
+        } else if (this.xPosition > ctx.canvas.width) {
 
+            this.xPosition = -this.width;
+        }
+    }
 }
 //end of class creation.....................
 //create all variables here:................
@@ -110,19 +137,18 @@ let counter = 0;
 
 function drawCharacter() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    console.log(player1.animate.frame)
     image.src = player1.animate.frame;
     ctx.drawImage(image, player1.xPosition, player1.yPosition, player1.width, player1.height);
 }
-
 //end of creation of variables..............
 //main loop function
 function loop() {
     player1.spirit();
     player1.animate.update();
     drawCharacter();
-    window.requestAnimationFrame(loop)
+    window.requestAnimationFrame(loop);
 }
-
 //create all eventlisteners here
 window.addEventListener("load", (event) => {
     window.requestAnimationFrame(loop);
