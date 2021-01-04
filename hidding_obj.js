668a7cdf8ec3a1a8a88b2b6063e8d90499817e30
1feby <feby.gergis97@eng-st.cu.edu.eg>
2021-01-04 12:44:00 +0200

    object to get rewarded

--- hidding_obj.js
+++ hidding_obj.js
@@ -1,6 +1,9 @@
 var imag = document.getElementById("santa");
-
-
+var gift = document.getElementsByClassName("gift");
+for(let i=0;i<gift.length;i++){
+    gift[i].style.bottom = '150px';
+    gift[i].style.right = (300 * (i+1))+'px'
+}
 window.addEventListener("keydown",keyDownFunc);
 
 imag.style.left =0,imag.style.bottom=0;
@@ -12,6 +15,12 @@ function keyDownFunc(event){
       // console.log(imag.style.left)
     }else if(event.keyCode === 37 &&  parseInt(imag.style.left)>=0){
         imag.style.left = parseInt(imag.style.left)-10 +'px';
+    }
+    for(let i=0;i<gift.length;i++){
+        if(gift[i].x === imag.x + imag.width){
+            gift[i].style.display = "none"
+           score_text.textContent ="Score :" + ++score;
+        }
     }
    
 }
